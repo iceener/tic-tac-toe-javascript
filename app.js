1fa3ed03a8a7d78e55c22fee5f8b9364af0df8d7
Adam Gospodarczyk <adam@overment.com>
2019-01-12 14:26:31 +0100

    Initialize repository

---
+++ app.js
@@ -0,0 +1,47 @@
+const PLAYER1 = 'fa-circle-o';
+const PLAYER2 = 'fa-times';
+let round = 1;
+const board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+const combinations = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+];
+
+const boxes = [...document.querySelectorAll('.box')];
+boxes.forEach(box => box.addEventListener('click', pick));
+
+function pick(event) {
+    const { row, column } = event.target.dataset;
+    const turn = round % 2 === 0 ? PLAYER2 : PLAYER1;
+    if (board[row][column] !== '') return;
+    event.target.classList.add(turn);
+    board[row][column] = turn;
+    round++;
+
+    console.log(check());
+}
+
+function check() {
+    const result = board.reduce((total, row) => total.concat(row));
+    let winner = null;
+    let moves = {
+        'fa-times': [],
+        'fa-circle-o': []
+    };
+    result.forEach((field, index) => moves[field] ? moves[field].push(index) : null);
+    combinations.forEach(combination => {
+        if (combination.every(index => moves[PLAYER1].indexOf(index) > -1)) {
+            winner = 'Winner: Player 1';
+        }
+        if (combination.every(index => moves[PLAYER2].indexOf(index) > -1)) {
+            winner = 'Winner: Player 2';
+        }
+    });
+
+    return winner;
+}
